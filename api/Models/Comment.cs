817997de3e9937f901f9bf502991418b1d36a13e
Vu Mai <vuanhmai.io@gmail.com>
2024-03-15 18:08:06 -0700

    Implemented Stock Models: - Stock Class - Comment Class

---
+++ api/Models/Comment.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        // int? is nullable int
+        // Key
+        public int? StockId { get; set; }
+        // Link to the stock
+        // Navigation property
+        public Stock? Stock { get; set; }
+    }
+}
---
+++ api/Models/Stock.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        // make sure it is a decimal(18, 2) for money (18 digits, 2 after decimal)
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+
+        // One to Many Relationship
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
