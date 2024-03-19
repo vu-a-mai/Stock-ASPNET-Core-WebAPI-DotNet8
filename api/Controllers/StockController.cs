391b98a205bffc90ffac1e3056c163e4eaaadc75
Vu Mai <vuanhmai.io@gmail.com>
2024-03-19 02:08:28 -0700

    Implemented DELETE API

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -81,5 +81,24 @@ namespace api.Controllers
 
             return Ok(stockModel.ToStockDto());
         }
+
+        // DELETE method
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.Find(id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+
+            // return NoContent() because we are deleting a single resource
+            return NoContent();
+        }
     }
 }
