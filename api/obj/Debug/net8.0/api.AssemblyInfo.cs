b2afeceef0f3731b3e2db0659027bd86b31bbe44
Vu Mai <vuanhmai.io@gmail.com>
2024-03-18 20:36:02 -0700

    Implement StockDtos and StockMappers

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8616e6a828e06f28bd4a528e801db234165e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fa6bebd1fd400dfabab5a02ea23e6d25f532d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
